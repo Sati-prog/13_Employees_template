2e84539f660772eb720d230fe4222ffa7838c075
Sati <rock_sal@mail.ru>
2022-07-26 20:06:21 +0600

    Rise of the state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,11 +15,11 @@ class App extends Component {
         this.state = {
             data: [
 
-                {name: 'John S.', salary: 2000, increase: false, id: 1},
-                {name: 'Alex M.', salary: 900, increase: true, id: 2},
-                {name: 'Carl W.', salary: 5000, increase: false, id: 3},
-                {name: 'Helen A.', salary: 3000, increase: false, id: 4},
-                {name: 'Mike U.', salary: 1300, increase: false, id: 5}
+                {name: 'John S.', salary: 2000, increase: false, rise: false, id: 1},
+                {name: 'Alex M.', salary: 900, increase: true, rise: false, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, rise: true, id: 3},
+                {name: 'Helen A.', salary: 3000, increase: false, rise: false, id: 4},
+                {name: 'Mike U.', salary: 1300, increase: false, rise: false, id: 5}
             ]
         }
         this.maxId = 6;
@@ -28,15 +28,6 @@ class App extends Component {
     deleteItem = (id) => {
 
         this.setState(({data}) => {
-
-            // const index = data.findIndex(elem => elem.id === id);
-            
-            // const before = data.slice(0, index);
-            // const after = data.slice(index + 1);
-
-            // const newArr = [...before, ...after];
-
-            // второй способ
 
             return {
                 data: data.filter(item => item.id !== id)
@@ -51,6 +42,7 @@ class App extends Component {
             name, 
             salary,
             increase: false,
+            rise: false,
             id: this.maxId++
         }
 
@@ -65,11 +57,82 @@ class App extends Component {
         });
     }
 
+    // onToggleIncrease = (id) => {
+
+    //     // this.setState(({data}) => {
+
+    //     //     const index = data.findIndex(elem => elem.id === id);
+
+    //     //     const old = data[index],
+    //     //           newItem = {...old, increase: !old.increase},
+    //     //           newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+    //     //     return {
+
+    //     //         data: newArr
+    //     //     }
+    //     // });
+
+    //     // другой вариант
+
+    //     this.setState(({data}) => ({
+
+    //         data: data.map(item => {
+
+    //             if (item.id === id) {
+
+    //                 return {...item, increase: !item.increase}
+    //             }
+
+    //             return item;
+    //         })
+    //     }));
+    // }
+
+    // onToggleRise = (id) => {
+
+    //     this.setState(({data}) => ({
+
+    //         data: data.map(item => {
+
+    //             if (item.id === id) {
+
+    //                 return {...item, rise: !item.rise}
+    //             }
+
+    //             return item;
+    //         })
+    //     }));
+    // }
+
+    // объединение 2-х методов
+
+    onToggleProp = (id, prop) => {
+
+        this.setState(({data}) => ({
+
+            data: data.map(item => {
+
+                if (item.id === id) {
+
+                    return {...item, [prop]: !item[prop]}
+                }
+
+                return item;
+            })
+        }));
+    }
+
     render() {
+
+        const employees = this.state.data.length,
+              increased = this.state.data.filter(item => item.increase).length;
 
         return (
             <div className="app">
-                <AppInfo/>
+                <AppInfo
+                    employees={employees}
+                    increased={increased}/>
     
                 <div className="search-panel">
                     <SearchPanel/>
@@ -78,7 +141,8 @@ class App extends Component {
     
                 <EmployeesList 
                     data={this.state.data}
-                    onDelete={this.deleteItem}/>
+                    onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -25,6 +25,8 @@ class EmployeesAddForm extends Component {
     onSubmit = (e) => {
 
         e.preventDefault();
+
+        if (this.state.name.length < 3 || !this.state.salary) return;
         this.props.onAdd(this.state.name, this.state.salary);
         this.setState({
             
